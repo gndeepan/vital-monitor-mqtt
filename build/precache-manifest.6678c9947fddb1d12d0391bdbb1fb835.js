51745519e130ffc37bbbc4ea9c0ff03b94d6e969
DeepanRajG <109058500+DeepanRajG@users.noreply.github.com>
2022-08-30 14:31:53 +0530

    final

---
+++ build/precache-manifest.6678c9947fddb1d12d0391bdbb1fb835.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "edcd1f4950f151dbf35d",
+    "url": "/vital-monitor-mqtt/static/css/main.720f081f.chunk.css"
+  },
+  {
+    "revision": "edcd1f4950f151dbf35d",
+    "url": "/vital-monitor-mqtt/static/js/main.edcd1f49.chunk.js"
+  },
+  {
+    "revision": "4567e6b4a7bd4ab0b748",
+    "url": "/vital-monitor-mqtt/static/js/1.4567e6b4.chunk.js"
+  },
+  {
+    "revision": "6d236caa8da518f7e582",
+    "url": "/vital-monitor-mqtt/static/js/2.6d236caa.chunk.js"
+  },
+  {
+    "revision": "b878f8b180917d4426c0",
+    "url": "/vital-monitor-mqtt/static/js/runtime~main.b878f8b1.js"
+  },
+  {
+    "revision": "1a83337fb80bad1fd99d950696fb894f",
+    "url": "/vital-monitor-mqtt/index.html"
+  }
+];
